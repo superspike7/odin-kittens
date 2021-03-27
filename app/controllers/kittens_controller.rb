74c2809439a3feb42bd53424c1baf80b05f8cdd6
Spike Vinz Cruz <spikevinz@gmail.com>
2021-03-27 08:49:32 +0800

    add CRUD methods

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -9,16 +9,44 @@ class KittensController < ApplicationController
   end
 
   def edit
+    @kitten = Kitten.find(params[:id])
   end
 
-  def delete
+  def destroy 
+    @kitten= Kitten.find(params[:id])
+    @kitten.destroy
 
+    redirect_to root_path
   end
 
   def update
+    @kitten = Kitten.find(params[:id])
+
+    if @kitten.update(kitten_params)
+       redirect_to @kitten
+    else
+        render 'edit', status: :unprocessable_entity
+    end
+
   end
 
   def create
+    @kitten = Kitten.new(kitten_params)
+    if @kitten.save
+      redirect_to root_path
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @kitten = Kitten.find(params[:id])
+  end
+
+private 
+
+  def kitten_params
+    params.require(:kitten).permit(:name, :age, :cuteness, :softness)
   end
 
 end
